8d1009c188ca36734d8bb871d6de6468c79ccd59
Devam Sisodraker <devam@sisodraker.com>
2021-01-09 21:04:02 -0800

    doesnt work anymore :(

---
+++ js/interval.js
@@ -0,0 +1,55 @@
+
+class CheckInterval {
+
+    /**
+     * Check every 1000ms by default
+     */
+    checkIntervalGap = 1000;
+
+    /**
+     * Actual reference to interval object
+     */
+    checkInterval;
+
+    constructor() {
+        checkInterval = setInterval(() => this.onCheckInterval(), this.checkIntervalGap);
+    }
+
+    /**
+     * Set the check interval
+     * @param {number} value the check interval (in terms of ms)
+     */
+    setCheckInterval(value) {
+        this.checkInterval = value;
+        clearInterval(this.checkInterval);
+        this.checkInterval = setInterval(() => this.checkInterval(), this.checkInterval);
+    }
+
+    /**
+     * Event run when the check interval must occur
+     */
+    onCheckInterval() {
+        this.isGoodPosition();
+    }
+
+    /**
+     * Function which determines if the posture is good
+     */
+    isGoodPosition() {
+        posturenn.classifyRightNow()
+            .then((result) => {
+                if (result == "BAD") {
+                    this.displayBadPostureMessage();
+                }
+            })
+            .catch((error) => {});
+    }
+
+    /**
+     * Function which is execute when the NN detects bad posture
+     */
+    displayBadPostureMessage(); // stub
+
+}
+
+// const checkInterval = new CheckInterval();
